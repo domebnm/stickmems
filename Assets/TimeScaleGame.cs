e1680d32c59c155d4aea9aa32453467aa7c640f5
wowyomad <vadimsun8837@gmail.com>
2023-09-30 20:59:54 +0300

    removed prints

--- Assets/TimeScaleGame.cs
+++ Assets/TimeScaleGame.cs
@@ -21,26 +21,20 @@ public class TimeScaleGame : MonoBehaviour
     static float time = 3.0f;
     static float slowTimeScale = 0.1f;
     static float originalFixedDeltaTime = Time.fixedDeltaTime;
-
     static bool isRunning;
-
 
     public static IEnumerator TimeScale()
     {
         if(isRunning) { yield break; }
         isRunning = true;
-        print("before: " + Time.fixedDeltaTime + ", " + Time.timeScale);
 
         Time.timeScale = slowTimeScale;
         Time.fixedDeltaTime = originalFixedDeltaTime * slowTimeScale; // Adjust the fixed time step
-        print("when: " + Time.fixedDeltaTime + ", " + Time.timeScale);
 
         yield return new WaitForSeconds(time * slowTimeScale);
         Time.timeScale = 1.0f;
-        print("on courutine, originalFixedDeltaTime: " + originalFixedDeltaTime);
 
         Time.fixedDeltaTime = originalFixedDeltaTime; // Restore the original fixed time step
-        print("after: " + Time.fixedDeltaTime + ", " + Time.timeScale);
         isRunning = false;
         yield return null;
     }
