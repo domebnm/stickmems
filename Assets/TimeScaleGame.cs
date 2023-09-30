c279a990125c4f4cabc0090c64897223989a5afe
sundukovvadim1 <sundukovvadim1@gmail.com>
2023-09-30 20:50:09 +0300

    removed prints in TimeScaleGame.cs

--- Assets/TimeScaleGame.cs
+++ Assets/TimeScaleGame.cs
@@ -29,18 +29,14 @@ public class TimeScaleGame : MonoBehaviour
     {
         if(isRunning) { yield break; }
         isRunning = true;
-        print("before: " + Time.fixedDeltaTime + ", " + Time.timeScale);
 
         Time.timeScale = slowTimeScale;
         Time.fixedDeltaTime = originalFixedDeltaTime * slowTimeScale; // Adjust the fixed time step
-        print("when: " + Time.fixedDeltaTime + ", " + Time.timeScale);
 
         yield return new WaitForSeconds(time * slowTimeScale);
         Time.timeScale = 1.0f;
-        print("on courutine, originalFixedDeltaTime: " + originalFixedDeltaTime);
 
         Time.fixedDeltaTime = originalFixedDeltaTime; // Restore the original fixed time step
-        print("after: " + Time.fixedDeltaTime + ", " + Time.timeScale);
         isRunning = false;
         yield return null;
     }
